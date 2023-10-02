5bcc00143220b1e15296caccfa16f7ff2f5a0441
Mohammad Yusuf <118352964+mohd1612yusuf@users.noreply.github.com>
2023-10-02 18:18:56 +0530

    Add files via upload

---
+++ S1V6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cout<<"Input 4 integers a, b, c and d"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b and c>d);
+    return 0;
+}
